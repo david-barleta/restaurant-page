b6d7b0c11ed96ef2dc25902d56acce45dd1323a0
David <davidjoshua285@gmail.com>
2023-07-22 07:55:25 +0800

    Create a module that loads the home page

---
+++ src/home.js
@@ -0,0 +1,69 @@
+export default function loadHomePage() {
+  const homeDiv = document.createElement('div');
+  homeDiv.id = 'home';
+
+  const heroSection = document.createElement('div');
+  heroSection.id = 'hero-section';
+
+  const header = document.createElement('header');
+
+  const emptyDiv = document.createElement('div');
+
+  const headerLogo = document.createElement('div');
+  headerLogo.textContent = 'Café Luna';
+
+  const homeBtn = document.createElement('button');
+  homeBtn.type = 'button';
+  homeBtn.id = 'home-button';
+  homeBtn.textContent = 'Home';
+
+  const menuBtn = document.createElement('button');
+  menuBtn.type = 'button';
+  menuBtn.id = 'menu-button';
+  menuBtn.textContent = 'Menu';
+
+  const contactBtn = document.createElement('button');
+  contactBtn.type = 'button';
+  contactBtn.id = 'contact-button';
+  contactBtn.textContent = 'Contact';
+
+  header.append(emptyDiv, headerLogo, homeBtn, menuBtn, contactBtn)
+
+  const heroContent = document.createElement('div');
+  heroContent.id = 'hero-content';
+
+  const heroContentLogo = document.createElement('div');
+  heroContentLogo.textContent = 'Café Luna';
+
+  const heroContentIntro = document.createElement('p');
+  heroContentIntro.textContent = 'Welcome to Café Luna, a place where you can unwind and enjoy delicious flavors. Located in the heart of Cavite, our peaceful cafe offers a break from the busyness of daily life. Whether you\'re seeking solitude or spending time with friends, our cozy interior sets the perfect atmosphere. With soft lighting, comfortable seating, and soothing music, you can unwind, reflect, and connect with others.';
+
+  heroContent.append(heroContentLogo, heroContentIntro);
+
+  heroSection.append(header, heroContent);
+
+  const menuSection = document.createElement('div');
+  menuSection.id = 'menu-section';
+
+  const menuDescription = document.createElement('p');
+  menuDescription.textContent = 'At Café Luna, we take pride in offering a range of menu options that strike a balance between quality and affordability. We\'ve carefully created a menu that caters to your desire for both relaxation and culinary pleasure. Indulge in the enticing aroma of freshly brewed coffee drinks, expertly prepared by our skilled baristas. Treat yourself to our delightful selection of pastries and cakes, each baked to perfection, tempting you wth their delightful textures and flavors.';
+
+  menuSection.append(menuDescription);
+
+  const ctaSection = document.createElement('div');
+  ctaSection.id = 'cta-section';
+
+  const cta = document.createElement('p');
+  cta.textContent = 'Experience the perfect blend of flavors, relaxation, and connection at Café Luna.';
+
+  const ctaBtn = document.createElement('button');
+  ctaBtn.type = 'button';
+  ctaBtn.textContent = 'View our menu.'
+
+  ctaSection.append(cta, ctaBtn);
+
+  homeDiv.append(heroSection, menuSection, ctaSection);
+
+  const pageContent = document.querySelector('#content');
+  pageContent.append(homeDiv);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("This is a restaurant page")
+import loadHomePage from "./home.js";
+
+function clearCurrentPage() {
+  const content = document.querySelector('#content');
+  content.innerHTML = '';
+  console.log('I was clicked')
+}
+
+function bindHeaderBtns() {
+  const homeBtn = document.querySelector('#home-button');
+  const menuBtn = document.querySelector('#menu-button');
+  const contactBtn = document.querySelector('#contact-button');
+
+  homeBtn.addEventListener('click', clearCurrentPage);
+  homeBtn.addEventListener('click', loadHomePage);
+}
+
+loadHomePage();
